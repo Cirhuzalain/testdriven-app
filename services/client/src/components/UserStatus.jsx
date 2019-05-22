2acd04f7fa51d833bcc0ea15e28f7e49df9834f6
Alino <cirhuzalain@gmail.com>
2019-05-22 11:05:13 +0200

    Add admin state to client side component

--- services/client/src/components/UserStatus.jsx
+++ services/client/src/components/UserStatus.jsx
@@ -8,7 +8,9 @@ class UserStatus extends Component {
     this.state = {
       email: "",
       id: "",
-      username: ""
+      username: "",
+      active: "", // new
+      admin: "" // new
     };
   }
   componentDidMount() {
@@ -30,7 +32,9 @@ class UserStatus extends Component {
         this.setState({
           email: res.data.data.email,
           id: res.data.data.id,
-          username: res.data.data.username
+          username: res.data.data.username,
+          active: String(res.data.data.active), // new
+          admin: String(res.data.data.admin) // new
         });
       })
       .catch(error => {
@@ -58,6 +62,14 @@ class UserStatus extends Component {
           <li>
             <strong>Username:</strong> {this.state.username}
           </li>
+          <li>
+            <strong>Active:</strong> {this.state.active}
+          </li>{" "}
+          {/* new */}
+          <li>
+            <strong>Admin:</strong> {this.state.admin}
+          </li>{" "}
+          {/* new */}
         </ul>
       </div>
     );
