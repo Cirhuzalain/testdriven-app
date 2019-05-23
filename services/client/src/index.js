aa5575e1ecbb5409f66af2d9a9dda48ba726734f
Alino <cirhuzalain@gmail.com>
2019-05-23 12:55:11 +0200

    Add a script for end2end tests

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom"; // new
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App.jsx";
 
